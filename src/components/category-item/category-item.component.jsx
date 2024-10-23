dda28048369ba01f6e160be5d654b1f7bb3529cb
Yekoko <yekokooo1991@gmail.com>
2024-10-23 15:48:52 +0900

    added category components

---
+++ src/components/category-item/category-item.component.jsx
@@ -0,0 +1,16 @@
+import "./category-item.style.scss";
+
+const Category = ({category}) => {
+  const { title, imageUrl } = category;
+  return (
+    <div className="category-container">
+      <div className="background-image" style={{ backgroundImage: `url(${imageUrl})`}}/>
+      <div className="category-body-container">
+        <h2>{title}</h2>
+        <p>Shop Now</p>
+      </div>
+    </div>
+  );
+};
+
+export default Category;
---
+++ src/components/category-list/category-list.component.jsx
@@ -0,0 +1,16 @@
+import CategoryItem from "../category-item/category-item.component";
+import "./category-list.style.scss";
+
+const CategoryList = ({ categories }) => {
+  return (
+    <div className="categories-container">
+      {categories.map((category) => {
+        return (
+          <CategoryItem key={category.id} category={category} />
+        );
+      })}
+    </div>
+  );
+};
+
+export default CategoryList;
